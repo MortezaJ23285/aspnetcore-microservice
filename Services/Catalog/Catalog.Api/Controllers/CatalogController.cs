9783daf863e18ee6abcbff74b9d429dd90527fb7
Morteza Javaheri <morteza.j.23285@gmail.com>
2025-05-10 15:32:08 +0330

    w3rtg4

---
+++ Services/Catalog/Catalog.Api/Controllers/CatalogController.cs
@@ -0,0 +1,69 @@
+﻿using Catalog.Api.Entities;
+using Catalog.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class CatalogController : ControllerBase
+{
+    private readonly IProductRepository _productRepository;
+    private readonly ILogger<CatalogController> _logger;
+
+    public CatalogController(IProductRepository productRepository, ILogger<CatalogController> logger)
+    {
+        _productRepository = productRepository;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        => Ok(await _productRepository.GetProducts());
+
+    [HttpGet("{id:length(24)}", Name = "GetProduct")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts(string id)
+    {
+        var product = await _productRepository.GetProduct(id);
+
+        if (product == null)
+        {
+            _logger.LogInformation($"Product with id: {id} not found");
+            return NotFound();
+        }
+
+        return Ok(product);
+    }
+
+    [HttpGet("[action]/{category}")]
+    [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProductsByCategory(string category)
+        => Ok(await _productRepository.GetProductsByCategory(category));
+
+
+    [HttpPost]
+    [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+    {
+        await _productRepository.CreateProduct(product);
+        return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+    }
+
+    [HttpPut]
+    [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+    {
+        return Ok(await _productRepository.UpdateProduct(product));
+    }
+
+
+    [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+    [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> DeleteProduct(string id)
+    {
+        return Ok(await _productRepository.DeleteProduct(id));
+    }
+}
---
+++ Services/Catalog/Catalog.Api/Data/CatalogContextSeed.cs
@@ -0,0 +1,52 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data;
+
+public class CatalogContextSeed
+{
+    public static void SeedData(IMongoCollection<Product> productCollection)
+    {
+        bool existProduct = productCollection.Find(p => true).Any();
+
+        if (!existProduct)
+        {
+            productCollection.InsertMany(GetSeedData());
+        }
+    }
+
+    private static IEnumerable<Product> GetSeedData() =>
+        new List<Product>
+        {
+            new()
+            {
+                Id = "6808bcc7d1051679fba9b476",
+                Category = "Phone",
+                Name = "Phone X",
+                Description = "Phone X",
+                Price = 850000,
+                Summary = "Phone X",
+                ImageName = "Phone X",
+            },
+            new()
+            {
+                Id = "6808bcc9d1051679fba9b478",
+                Category = "Phone",
+                Name = "Phone Y",
+                Description = "Phone Y",
+                Price = 250000,
+                Summary = "Phone Y",
+                ImageName = "Phone Y",
+            },
+            new()
+            {
+                Id = "6808c50f0812f9073f4fe204",
+                Category = "Phone",
+                Name = "Phone Z",
+                Description = "Phone Z",
+                Price = 150000,
+                Summary = "Phone Z",
+                ImageName = "Phone Z",
+            }
+        };
+}
