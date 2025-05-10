217c7c5e29b42a213129635a88820001b4abbd5b
Morteza <morteza.j.23285@gmail.com>
2025-05-10 23:18:57 +0330

    edvsv

---
+++ Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿namespace Basket.Api.Entities;
+
+public class ShoppingCart
+{
+    public ShoppingCart()
+    {
+    }
+
+    public ShoppingCart(string username)
+    {
+        Username = username;
+    }
+
+    public string Username { get; set; }
+
+    public List<ShoppingCartItem> Items { get; set; }
+
+    public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
+}
