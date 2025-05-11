d293738b8c7e0d3422d2da8cca92ed27830837e6
Morteza Javaheri <morteza.j.23285@gmail.com>
2025-05-11 16:17:44 +0330

    rgrgrg

---
+++ Services/Discount/Discount.Api/Controllers/DiscountController.cs
@@ -0,0 +1,48 @@
+﻿using Discount.Api.Entities;
+using Discount.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.Api.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class DiscountController : ControllerBase
+{
+    private readonly IDiscountRepository _discountRepository;
+
+    public DiscountController(IDiscountRepository discountRepository)
+    {
+        _discountRepository = discountRepository;
+    }
+
+    [HttpGet("{productName}", Name = "GetDiscount")]
+    [ProducesResponseType(typeof(Coupon), StatusCodes.Status200OK)]
+    public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+    {
+        var discount = await _discountRepository.GetDiscount(productName);
+        return Ok(discount);
+    }
+
+
+    [HttpPost]
+    [ProducesResponseType(typeof(Coupon), StatusCodes.Status200OK)]
+    public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+    {
+        await _discountRepository.CreateDiscount(coupon);
+        return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+    }
+
+    [HttpPut]
+    [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+    public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+    {
+        return Ok(await _discountRepository.UpdateDiscount(coupon));
+    }
+
+    [HttpDelete]
+    [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+    public async Task<ActionResult<Coupon>> UpdateDiscount(string productName)
+    {
+        return Ok(await _discountRepository.DeleteDiscount(productName));
+    }
+}
---
+++ Services/Discount/Discount.Api/Repositories/DiscountRepository.cs
@@ -0,0 +1,60 @@
+﻿using Dapper;
+using Discount.Api.Entities;
+using Npgsql;
+
+namespace Discount.Api.Repositories;
+
+public class DiscountRepository : IDiscountRepository
+{
+    private readonly IConfiguration _configuration;
+
+    public DiscountRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+
+    public async Task<Coupon> GetDiscount(string productName)
+    {
+        await using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        var result = await connection.QueryFirstOrDefaultAsync<Coupon>(
+            "SELECT * FROM Coupon WHERE ProductName = @ProductName", new { ProductName = productName });
+
+        return result ?? new();
+    }
+
+    public async Task<bool> CreateDiscount(Coupon coupon)
+    {
+        await using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        var affect = await connection.ExecuteAsync(
+            "INSERT INTO Coupon (ProductName, Description, Amount) VALUES (@ProductName, @Description, @Amount)", new
+            {
+                ProductName = coupon.ProductName,
+                Description = coupon.Description,
+                Amount = coupon.Amount
+            });
+        return affect > 0;
+    }
+
+    public async Task<bool> UpdateDiscount(Coupon coupon)
+    {
+        await using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        var affect = await connection.ExecuteAsync(
+            "UPDATE Coupon SET ProductName=@ProductName, Description=@Description , Amount=@Amount",
+            new
+            {
+                ProductName = coupon.ProductName,
+                Description = coupon.Description,
+                Amount = coupon.Amount
+            });
+        return affect > 0;
+    }
+
+    public async Task<bool> DeleteDiscount(string productName)
+    {
+        await using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        var affect = await connection.ExecuteAsync(
+            "DELETE FROM Coupon WHERE ProductName = @ProductName", new { ProductName = productName });
+        return affect > 0;
+    }
+}
